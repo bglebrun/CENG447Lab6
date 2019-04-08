2ca10cd5b0ad11796b086028728c766aedb84f02
Benjamin LeBrun <bl.lebrun@gmail.com>
2019-04-07 20:35:06 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#define F_CPU 16000000
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+void Init()
+{
+    // Enable global interrupts
+    sei();
+}
+
+int main()
+{
+    Init();
+    while (1)
+    {
+
+    }
+    return 1;
+}
