1dd2a53fc38d9438f197078e2bd10d6f8ba47912
Benjamin Garcia <benjamin.garcia@mines.sdsmt.edu>
2019-04-09 16:02:03 -0600

    set OCR1B to 0 to match OCR1A

--- ultrasonic.c
+++ ultrasonic.c
@@ -4,10 +4,10 @@
  *
  * Ultrasonic sensor: A4 A5 (portc 4 5)
  * Servo Motor: 3 (portd 3)
- */ 
+ */
 
-void
-initUltrasonic() {
+void initUltrasonic()
+{
     // Trig on A5
     setBit(DDRC, US_TRIG);
     // Echo on A4
@@ -18,34 +18,38 @@ initUltrasonic() {
      * Pins 7:4 zeroes
      * tccr1a high byte zeroes
      * 16 us
-     */ 
+     */
     OCR1A = 0;
+    OCR1B = 0;
     // Start tcount at 1
     TCNT1 = 0x00;
     // Normal mode
     TCCR1A = 0x00;
-    /* 
+    /*
      * Prescaler value goes here
      * 1 - no prescaling
      * 2 - clock /8
      * 3 - clock /64
      * 4 - clock /256 - 16us
      * 5 - clock /1024 - 64us
-     * source - https://sites.google.com/site/qeewiki/books/avr-guide/timers-on-the-atmega328
+     * source -
+     * https://sites.google.com/site/qeewiki/books/avr-guide/timers-on-the-atmega328
      */
     TCCR1B = 0x05;
 }
 
-ISR(TIMER1_CAPT_vect) {
+ISR(TIMER1_CAPT_vect)
+{
     // Timer 1 capture
 }
 
-ISR(TIMER1_OVF_vect) {
+ISR(TIMER1_OVF_vect)
+{
     // Timer 1 overflow
 }
 
-unsigned int
-TIM16_ReadTCNT0() {
+unsigned int TIM16_ReadTCNT0()
+{
     unsigned char sreg;
     unsigned int i;
     // Save global interrupt flag
@@ -59,8 +63,8 @@ TIM16_ReadTCNT0() {
     return i;
 }
 
-unsigned int
-TIM16_ReadTCNT1() {
+unsigned int TIM16_ReadTCNT1()
+{
     unsigned char sreg;
     unsigned int i;
     // Save global interrupt flag
@@ -74,8 +78,8 @@ TIM16_ReadTCNT1() {
     return i;
 }
 
-void
-TIM16_WriteTCNT0( unsigned int i ) {
+void TIM16_WriteTCNT0(unsigned int i)
+{
     unsigned char sreg;
     unsigned int i;
     // Save global interrupt flag
@@ -88,8 +92,8 @@ TIM16_WriteTCNT0( unsigned int i ) {
     SREG = sreg;
 }
 
-void
-TIM16_WriteTCNT1( unsigned int i ) {
+void TIM16_WriteTCNT1(unsigned int i)
+{
     unsigned char sreg;
     unsigned int i;
     // Save global interrupt flag
